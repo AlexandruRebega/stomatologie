8b52797fec538572c4dca7416f42cbde1048c957
AlexRebega <rebega.alex@gmail.com>
2019-12-29 23:48:23 +0200

    Index page

--- py/app.py
+++ py/app.py
@@ -102,7 +102,7 @@ def signupHandler():
 
 @app.route('/')
 def index(): 
-    return loginHandler()
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
